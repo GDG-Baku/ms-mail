3781276d6cc0a113ffcc52b5fbc738ec02e1bb9b
alihsoff <alihsoff@gmail.com>
2020-05-02 01:25:18 +0400

    BE-7: set mail sender name

--- src/main/java/az/gdg/msmail/service/MailServiceImpl.java
+++ src/main/java/az/gdg/msmail/service/MailServiceImpl.java
@@ -5,6 +5,7 @@ import az.gdg.msmail.util.MailUtil;
 import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
@@ -13,6 +14,7 @@ import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+import java.io.UnsupportedEncodingException;
 
 @Service
 @AllArgsConstructor
@@ -20,19 +22,23 @@ public class MailServiceImpl implements MailService {
 
     private static final Logger logger = LoggerFactory.getLogger(MailServiceImpl.class);
     private final JavaMailSender sender;
-
+    @Value("${spring.mail.username}")
+    private final String senderMail;
 
     public void processMailRequest(MailDTO mailDTO) {
         logger.info("ActionLog.processMailRequest.start");
         MimeMessage message = sender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(message, "utf-8");
         try {
+            message.setFrom(new InternetAddress(senderMail, "Rubber Duck"));
             helper.setText(mailDTO.getBody(), true);
             helper.setSubject(mailDTO.getSubject());
             InternetAddress[] recipients = InternetAddress.parse(MailUtil.listToString(mailDTO.getTo()));
             message.setRecipients(Message.RecipientType.TO, recipients);
         } catch (MessagingException e) {
-            logger.error("ActionLog.MessagingException.Thrown", e);
+            logger.error("Thrown.MessagingException.Thrown", e);
+        } catch (UnsupportedEncodingException e) {
+            logger.error("Thrown.UnsupportedEncodingException.Thrown", e);
         }
         sender.send(message);
         logger.info("ActionLog.processMailRequest.end");
